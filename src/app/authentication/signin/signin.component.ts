cb10ad221c0c3283d813567c476f72bb21aaa955
R P Buvan Varun <buvanvarun.acad@gmail.com>
2020-07-28 01:51:16 +0530

    cleaned signin,signup

--- src/app/authentication/signin/signin.component.ts
+++ src/app/authentication/signin/signin.component.ts
@@ -7,120 +7,36 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./signin.component.css'],
 })
 export class SigninComponent implements OnInit {
-  sections;
   visible = false;
-  index: number = 0;
-  animationDuration = 1700;
-  lastTime = 0;
-  event;
+  event: HTMLInputElement;
   email: string = '';
   password: string = '';
   visibleBtn = false;
+  el: string;
+  showEmail_signin: boolean = true;
+  showPassword_signin: boolean = false;
   constructor(private route: ActivatedRoute, private router: Router) {}
 
-  ngOnInit(): void {
-    this.sections = document.querySelectorAll('section');
-    // this.sections.forEach((section, i) => {
-    //   if (i === 0) {
-    //     section.querySelector('.section-wrapper').classList.add('show');
-    //   }
-    // });
-    this.scrollSection();
-    this.keyDown();
-  }
+  ngOnInit(): void {}
 
   onVisible = () => {
     this.visible = this.visible === true ? false : true;
     this.visibleBtn = this.visibleBtn === true ? false : true;
   };
 
-  toggleSection = (index, state) => {
-    if (state === 'show') {
-      this.sections.forEach((section, i) => {
-        if (i === index) {
-          section.querySelector('.section-wrapper').classList.add('show');
-        }
-      });
+  keyDown(event: Event) {
+    this.el = (<HTMLInputElement>event.target).name;
+
+    if (this.el === 'email') {
+      this.showEmail_signin = false;
+      this.showPassword_signin = true;
+    } else if (this.el === 'password') {
+      this.onSignIn();
     } else {
-      this.sections.forEach((section, i) => {
-        if (i === index) {
-          section.querySelector('.section-wrapper').classList.remove('show');
-        }
-      });
+      return;
     }
-  };
+  }
 
-  scrollSection = () => {
-    window.addEventListener(
-      'wheel',
-      (e) => {
-        const delta = e.deltaY;
-        const currentTime = new Date().getTime();
-
-        if (currentTime - this.lastTime < this.animationDuration) {
-          e.preventDefault();
-          return;
-        }
-        if (delta > 0) {
-          // down wheel
-          if (this.index >= 1) return;
-          this.toggleSection(this.index, 'hide');
-          this.index++;
-          this.sections.forEach((section, i) => {
-            if (i === this.index) {
-              this.toggleSection(i, 'show');
-              section.scrollIntoView({ behaviour: 'smooth' });
-            }
-          });
-        } else {
-          // up wheel
-          if (this.index < 1) return;
-          this.toggleSection(this.index, 'hide');
-          this.index--;
-          this.sections.forEach((section, i) => {
-            if (i === this.index) {
-              this.toggleSection(i, 'show');
-              section.scrollIntoView({ behaviour: 'smooth' });
-            }
-          });
-        }
-        this.lastTime = currentTime;
-      },
-      { passive: false }
-    );
-  };
-
-  keyDown = () => {
-    window.addEventListener(
-      'keydown',
-      (e) => {
-        if (e.keyCode === 13) {
-          const currentTime = new Date().getTime();
-
-          if (currentTime - this.lastTime < this.animationDuration) {
-            e.preventDefault();
-            return;
-          }
-          if (this.index >= 1) {
-            this.onSignIn();
-            return;
-          }
-
-          this.toggleSection(this.index, 'hide');
-          this.index++;
-          this.sections.forEach((section, i) => {
-            if (i === this.index) {
-              this.toggleSection(i, 'show');
-              section.scrollIntoView({ behaviour: 'smooth' });
-            }
-          });
-
-          this.lastTime = currentTime;
-        }
-      },
-      { passive: false }
-    );
-  };
   onSignIn = () => {
     // backend part of signin does here
     console.log(this.email, this.password);
--- src/app/authentication/signup/signup.component.ts
+++ src/app/authentication/signup/signup.component.ts
@@ -5,120 +5,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
-  sections;
   visible = false;
-  index: number = 0;
-  animationDuration = 1700;
-  lastTime = 0;
-  event;
+  event: HTMLInputElement;
   name: string = '';
   email: string = '';
   password: string = '';
   visibleBtn = false;
+  showName: boolean = true;
+  showEmail: boolean = false;
+  showPassword: boolean = false;
+  el: string;
   constructor() {}
 
-  ngOnInit(): void {
-    this.sections = document.querySelectorAll('section');
-    // this.sections.forEach((section, i) => {
-    //   if (i === 0) {
-    //     section.querySelector('.section-wrapper').classList.add('show');
-    //   }
-    // });
-    this.scrollSection();
-    this.keyDown();
-  }
+  ngOnInit(): void {}
 
+  // for toggling the visibility of password button
   onVisible = () => {
     this.visible = this.visible === true ? false : true;
     this.visibleBtn = this.visibleBtn === true ? false : true;
   };
 
-  toggleSection = (index, state) => {
-    if (state === 'show') {
-      this.sections.forEach((section, i) => {
-        if (i === index) {
-          section.querySelector('.section-wrapper').classList.add('show');
-        }
-      });
+  keyDown = (event: Event) => {
+    this.el = (<HTMLInputElement>event.target).name;
+    if (this.el === 'name') {
+      this.showName = false;
+      this.showEmail = true;
+    } else if (this.el === 'email') {
+      this.showEmail = false;
+      this.showPassword = true;
+    } else if (this.el === 'password') {
+      this.onSignUp();
     } else {
-      this.sections.forEach((section, i) => {
-        if (i === index) {
-          section.querySelector('.section-wrapper').classList.remove('show');
-        }
-      });
+      return;
     }
-  };
-
-  scrollSection = () => {
-    window.addEventListener(
-      'wheel',
-      (e) => {
-        const delta = e.deltaY;
-        const currentTime = new Date().getTime();
-
-        if (currentTime - this.lastTime < this.animationDuration) {
-          e.preventDefault();
-          return;
-        }
-        if (delta > 0) {
-          // down wheel
-          if (this.index >= 2) return;
-          this.toggleSection(this.index, 'hide');
-          this.index++;
-          this.sections.forEach((section, i) => {
-            if (i === this.index) {
-              this.toggleSection(i, 'show');
-              section.scrollIntoView({ behaviour: 'smooth' });
-            }
-          });
-        } else {
-          // up wheel
-          if (this.index < 1) return;
-          this.toggleSection(this.index, 'hide');
-          this.index--;
-          this.sections.forEach((section, i) => {
-            if (i === this.index) {
-              this.toggleSection(i, 'show');
-              section.scrollIntoView({ behaviour: 'smooth' });
-            }
-          });
-        }
-        this.lastTime = currentTime;
-      },
-      { passive: false }
-    );
-  };
-
-  keyDown = () => {
-    window.addEventListener(
-      'keydown',
-      (e) => {
-        if (e.keyCode === 13) {
-          const currentTime = new Date().getTime();
-
-          if (currentTime - this.lastTime < this.animationDuration) {
-            e.preventDefault();
-            return;
-          }
-          if (this.index >= 2) {
-            this.onSignUp();
-            return;
-          }
-
-          this.toggleSection(this.index, 'hide');
-          this.index++;
-          this.sections.forEach((section, i) => {
-            if (i === this.index) {
-              this.toggleSection(i, 'show');
-              section.scrollIntoView({ behaviour: 'smooth' });
-            }
-          });
-
-          this.lastTime = currentTime;
-        }
-      },
-      { passive: false }
-    );
   };
   onSignUp = () => {
     // backend part of signup can go here
